1e896635acf4dbb4524ee456cd26102ac2af13c7
zaheerhussain10 <zah82560@gmail.com>
2024-11-11 13:58:20 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,42 @@
 const decodeTheRing = function (s, p) {
+  // Recursive helper function to match the message and the pattern
+  const matchHelper = (sIndex, pIndex) => {
+      // If both the string and pattern are completely matched
+      if (sIndex === s.length && pIndex === p.length) {
+          return true;
+      }
 
-    // write your code here
+      // If the pattern has more characters but the string is finished
+      if (pIndex === p.length) {
+          return false;
+      }
 
+      // If current pattern character is '?', it can match any one character
+      if (p[pIndex] === '?') {
+          return matchHelper(sIndex + 1, pIndex + 1);
+      }
+
+      // If the current pattern character is '*', it can match zero or more characters
+      if (p[pIndex] === '*') {
+          // Try to match with zero characters or one character at a time
+          for (let i = sIndex; i <= s.length; i++) {
+              if (matchHelper(i, pIndex + 1)) {
+                  return true;
+              }
+          }
+          return false;
+      }
+
+      // If characters match exactly, move both indices
+      if (sIndex < s.length && s[sIndex] === p[pIndex]) {
+          return matchHelper(sIndex + 1, pIndex + 1);
+      }
+
+      // If no match, return false
+      return false;
   };
-  
-  module.exports = decodeTheRing;
+
+  return matchHelper(0, 0);
+};
+
+module.exports = decodeTheRing;
