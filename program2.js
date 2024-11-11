20e311b72dc0d477db57b02c927ddffdd85a2e03
zaheerhussain10 <zah82560@gmail.com>
2024-11-11 13:57:34 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,31 +1,7 @@
 const decodeTheRing = function (s, p) {
-  let i = 0, j = 0;
 
-  while (i < s.length && j < p.length) {
-    if (p[j] === '?') {
-      i++;
-      j++;
-    } else if (p[j] === '*') {
-      let starIdx = j;
-      while (j < p.length && p[j] === '*') {
-        j++;
-      }
-      if (j === p.length) {
-        return true; // Return true if the remaining message can be matched by remaining stars
-      }
-      while (i < s.length && s[i] !== p[j]) {
-        i++;
-      }
-    } else if (s[i] === p[j]) {
-      i++;
-      j++;
-    } else {
-      return false; // Return false if characters don't match and neither is a wildcard
-    }
-  }
+    // write your code here
 
-  // Return true if we've reached the end of the pattern and the message has been fully consumed
-  return j === p.length;
-};
-
-module.exports = decodeTheRing;
+  };
+  
+  module.exports = decodeTheRing;
