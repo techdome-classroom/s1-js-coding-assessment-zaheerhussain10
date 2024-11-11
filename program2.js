9e22d048e68c26da31f156bc886541cf4d0eec80
zaheerhussain10 <zah82560@gmail.com>
2024-11-11 13:47:50 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,30 @@
 const decodeTheRing = function (s, p) {
+  let i = 0, j = 0;
 
-    // write your code here
+  while (i < s.length && j < p.length) {
+    if (p[j] === '?') {
+      i++;
+      j++;
+    } else if (p[j] === '*') {
+      let starIdx = j;
+      while (j < p.length && p[j] === '*') {
+        j++;
+      }
+      if (j === p.length) {
+        return true;
+      }
+      while (i < s.length && s[i] !== p[j]) {
+        i++;
+      }
+    } else if (s[i] === p[j]) {
+      i++;
+      j++;
+    } else {
+      return false;
+    }
+  }
 
-  };
-  
-  module.exports = decodeTheRing;
+  return j === p.length;
+};
+
+module.exports = decodeTheRing;
